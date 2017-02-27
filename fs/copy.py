1e4ace87f17dab575f1eaa406deb041d9b724f47
Giampaolo Cimino <cimino@nurc.nato.int>
2017-02-27 17:01:42 +0100

    copy_if_newer param removed, implemented new methods for copy only when src file is newer

--- fs/copy.py
+++ fs/copy.py
@@ -15,7 +15,7 @@ from .path import frombase
 from .path import normpath
 
 
-def copy_fs(src_fs, dst_fs, walker=None, copy_if_newer=False):
+def copy_fs(src_fs, dst_fs, walker=None):
     """
     Copy the contents of one filesystem to another.
 
@@ -29,14 +29,15 @@ def copy_fs(src_fs, dst_fs, walker=None, copy_if_newer=False):
         in ``src_fs``. Set this if you only want to consider a sub-set
         of the resources in ``src_fs``.
     :type walker: :class:`~fs.walk.Walker`
-    :param copy_if_newer: Files are copied only if destination files don't exist or
-    sources are newer then destination files. Directories are always copied. In case time of modification for destination
-    files cannot be determined files are safely copied.
-    :type copy_if_newer: boolean
     :returns: a list with the copied files dst_path.
 
     """
-    return copy_dir(src_fs, '/', dst_fs, '/', walker=walker, copy_if_newer=copy_if_newer)
+    return copy_dir(src_fs, '/', dst_fs, '/', walker=walker)
+
+
+def copy_fs_if_newer(src_fs, dst_fs, walker=None):
+    return copy_dir_if_newer(src_fs, '/', dst_fs, '/', walker=walker)
+
 
 def _source_is_newer(src_fs, src_path, dst_fs, dst_path):
     """
@@ -58,14 +59,14 @@ def _source_is_newer(src_fs, src_path, dst_fs, dst_path):
         if not dst_fs.exists(dst_path):
             return True
         else:
-            return src_fs.getinfo(src_path, ('details', 'modified')).modified > dst_fs.getinfo(dst_path, ('details', 'modified')).modified
+            nmsp = ('details', 'modified')
+            return src_fs.getinfo(src_path, nmsp).modified > dst_fs.getinfo(dst_path, nmsp).modified
     except Exception as ex:
         #todo: should log something here
         #log.error("cannot determine if source file " + src_path + " is newer than destination file " + dst_path + ", thus safely copy the file')
         return True
 
-
-def copy_file(src_fs, src_path, dst_fs, dst_path, copy_if_newer=False):
+def copy_file(src_fs, src_path, dst_fs, dst_path):
     """
     Copy a file from one filesystem to another.
 
@@ -80,20 +81,28 @@ def copy_file(src_fs, src_path, dst_fs, dst_path, copy_if_newer=False):
     :type dst_fs: FS URL or instance
     :param dst_path: Path to a file on ``dst_fs``.
     :type dst_path: str
-    :returns: a list of the
-    :param copy_if_newer: File is copied only if destination file doesn't exists or
-    source is newer then destination file. In case time of modification for destination file cannot be determined file
-    is safely copied.
-    :type copy_if_newer: boolean
-    :returns: a the copied file dst_path or None.
-
 
     """
     with manage_fs(src_fs, writeable=False) as src_fs:
         with manage_fs(dst_fs, create=True) as dst_fs:
             if src_fs is dst_fs:
                 # Same filesystem, so we can do a potentially optimized copy
-                if not copy_if_newer or _source_is_newer(src_fs, src_path, dst_fs, dst_path):
+                src_fs.copy(src_path, dst_path, overwrite=True)
+                return dst_path
+            else:
+                # Standard copy
+                with src_fs.lock(), dst_fs.lock():
+                    with src_fs.open(src_path, 'rb') as read_file:
+                        # There may be an optimized copy available on dst_fs
+                        dst_fs.setbinfile(dst_path, read_file)
+                        return dst_path
+
+def copy_file_if_newer(src_fs, src_path, dst_fs, dst_path):
+    with manage_fs(src_fs, writeable=False) as src_fs:
+        with manage_fs(dst_fs, create=True) as dst_fs:
+            if src_fs is dst_fs:
+                # Same filesystem, so we can do a potentially optimized copy
+                if _source_is_newer(src_fs, src_path, dst_fs, dst_path):
                     src_fs.copy(src_path, dst_path, overwrite=True)
                     return dst_path
                 else:
@@ -101,7 +110,7 @@ def copy_file(src_fs, src_path, dst_fs, dst_path, copy_if_newer=False):
             else:
                 # Standard copy
                 with src_fs.lock(), dst_fs.lock():
-                    if not copy_if_newer or _source_is_newer(src_fs, src_path, dst_fs, dst_path):
+                    if _source_is_newer(src_fs, src_path, dst_fs, dst_path):
                         with src_fs.open(src_path, 'rb') as read_file:
                             # There may be an optimized copy available on dst_fs
                             dst_fs.setbinfile(dst_path, read_file)
@@ -121,10 +130,6 @@ def copy_structure(src_fs, dst_fs, walker=None):
         in ``src_fs``. Set this if you only want to consider a sub-set
         of the resources in ``src_fs``.
     :type walker: :class:`~fs.walk.Walker`
-    :param copy_if_newer: Files are copied only if destination files don't exist or
-    sources are newer then destination files. Directories are always copied. In case time of modification for destination
-    files cannot be determined files are safely copied.
-    :type copy_if_newer: boolean
 
     """
     walker = walker or Walker()
@@ -135,7 +140,7 @@ def copy_structure(src_fs, dst_fs, walker=None):
                     dst_fs.makedir(dir_path, recreate=True)
 
 
-def copy_dir(src_fs, src_path, dst_fs, dst_path, walker=None, copy_if_newer=False):
+def copy_dir(src_fs, src_path, dst_fs, dst_path, walker=None):
     """
     Copy a directory from one filesystem to another.
 
@@ -150,10 +155,6 @@ def copy_dir(src_fs, src_path, dst_fs, dst_path, walker=None, copy_if_newer=Fals
         in ``src_fs``. Set this if you only want to consider a sub-set
         of the resources in ``src_fs``.
     :type walker: :class:`~fs.walk.Walker`
-    :param copy_if_newer: Files are copied only if destination files don't exist or
-    sources are newer then destination files. Directories are always copied. In case time of modification for destination
-    files cannot be determined files are safely copied.
-    :type copy_if_newer: boolean
     :returns: a list with the copied files dst_path.
 
     """
@@ -181,9 +182,45 @@ def copy_dir(src_fs, src_path, dst_fs, dst_path, walker=None, copy_if_newer=Fals
                             src_fs,
                             info.make_path(dir_path),
                             dst_fs,
-                            info.make_path(copy_path),
-                            copy_if_newer=copy_if_newer
+                            info.make_path(copy_path)
                         )
                         if file_copied is not None:
                             files_copied.append(file_copied)
     return files_copied
+
+def copy_dir_if_newer(src_fs, src_path, dst_fs, dst_path, walker=None):
+    walker = walker or Walker()
+    _src_path = abspath(normpath(src_path))
+    _dst_path = abspath(normpath(dst_path))
+    files_copied = []
+    with manage_fs(src_fs, writeable=False) as src_fs:
+        with manage_fs(dst_fs, create=True) as dst_fs:
+            with src_fs.lock(), dst_fs.lock():
+                dst_fs.makedir(_dst_path, recreate=True)
+                nmsp = ('details', 'modified')
+                dst_state = {fp: i for fp, i in walker.info(dst_fs, _dst_path, nmsp) if i.is_file}
+                src_state = [(fp, i) for fp, i in walker.info(src_fs, _src_path, nmsp)]
+                for dir_path, copy_info in src_state:
+                    copy_path = combine(
+                        _dst_path,
+                        frombase(_src_path, dir_path)
+                    )
+                    if copy_info.is_dir:
+                        dst_fs.makedir(copy_path, recreate=True)
+                    if copy_info.is_file:
+                        if dir_path in dst_state:
+                            if copy_info.modified <= dst_state[dir_path].modified:
+                                #src not newer than dst => nothing to do
+                                continue
+                        #copy file, because src is newer or dst doesn't exist
+                        file_copied = copy_file(
+                            src_fs,
+                            dir_path,
+                            dst_fs,
+                            copy_path
+                        )
+                        if file_copied is not None:
+                            files_copied.append(file_copied)
+    return files_copied
+
+
--- tests/test_copy.py
+++ tests/test_copy.py
@@ -38,32 +38,57 @@ class TestCopy(unittest.TestCase):
         self.assertTrue(dst_fs.isdir('empty'))
         self.assertTrue(dst_fs.isfile('bar/baz.txt'))
 
+    def _create_sandbox_dir(self, prefix='pyfilesystem2_sandbox_', home=None):
+        if home is None:
+            return tempfile.mkdtemp(prefix=prefix)
+        else:
+            sandbox_path = os.path.join(home, prefix)
+            os.makedirs(sandbox_path, exist_ok=True)
+            return sandbox_path
 
-    def _create_dir(self, prefix='pyfilesystem2_tests_'):
-        return tempfile.mkdtemp(prefix=prefix)
-
-    def _create_file(self, filename, write_chars=1024):
-        dir = self._create_dir()
-        fn = os.path.join(dir, filename)
-        if write_chars > 0:
-            with open(fn, 'w') as f:
-                f.write('1' * write_chars)
-        return dir, fn
-
-    def test_copy_when_dst_doesnt_exist(self):
+    def _touch(self, root, filepath):
+        #create abs filename
+        abs_filepath = os.path.join(root, filepath)
+        #ensure dir exists, caution: exist_ok works only for python>=3.2
+        #os.makedirs(os.path.dirname(abs_filepath), exist_ok=True)
+        dirname = os.path.dirname(abs_filepath)
         try:
+            os.makedirs(dirname)
+        except OSError:
+            if os.path.exists(dirname):
+                pass
+            else:
+                raise
+        #touch file
+        with open(abs_filepath, 'a'):
+            os.utime(abs_filepath, None) #update the mtime in case the file exists, same as touch
+        return abs_filepath
 
-            filename = "file1.txt"
-            src_dir, _ = self._create_file(filename, 1024)
-            dst_dir, _ = self._create_file(filename, 0)
+    def _write_file(self, filepath, write_chars=1024):
+        with open(filepath, 'w') as f:
+            f.write('1' * write_chars)
+        return filepath
+
+    def test_copy_fs_if_newer_dst_older(self):
+        try:
+            #create first dst ==> dst is older the src ==> file should be copied
+            dst_dir = self._create_sandbox_dir()
+            dst_file1 = self._touch(dst_dir, "file1.txt")
+            self._write_file(dst_file1)
+
+            src_dir = self._create_sandbox_dir()
+            src_file1 = self._touch(src_dir, "file1.txt")
+            self._write_file(src_file1)
 
             src_fs = open_fs('osfs://' + src_dir)
             dst_fs = open_fs('osfs://' + dst_dir)
 
-            copied = fs.copy.copy_fs(src_fs, dst_fs, copy_if_newer=True)
+            self.assertTrue(dst_fs.exists("/file1.txt"))
 
-            self.assertEqual(copied, ["/" + filename])
-            self.assertTrue(dst_fs.exists(filename))
+            copied = fs.copy.copy_fs_if_newer(src_fs, dst_fs)
+
+            self.assertEqual(copied, ["/file1.txt"])
+            self.assertTrue(dst_fs.exists("/file1.txt"))
 
             src_fs.close()
             dst_fs.close()
@@ -72,22 +97,54 @@ class TestCopy(unittest.TestCase):
             shutil.rmtree(src_dir)
             shutil.rmtree(dst_dir)
 
-    def test_dont_copy_when_dst_exists(self):
+    def test_copy_fs_if_newer_when_dst_doesnt_exists(self):
         try:
+            src_dir = self._create_sandbox_dir()
+            src_file1 = self._touch(src_dir, "file1.txt")
+            self._write_file(src_file1)
 
-            filename = "file1.txt"
-            src_dir, _ = self._create_file(filename, 1024)
-            dst_dir, _ = self._create_file(filename, 1024)
+            src_file2 = self._touch(src_dir, "one_level_down" + os.sep + "file2.txt")
+            self._write_file(src_file2)
+
+            dst_dir = self._create_sandbox_dir()
 
             src_fs = open_fs('osfs://' + src_dir)
             dst_fs = open_fs('osfs://' + dst_dir)
 
-            self.assertTrue(dst_fs.exists(filename))
 
-            copied = fs.copy.copy_fs(src_fs, dst_fs, copy_if_newer=True)
+            copied = fs.copy.copy_fs_if_newer(src_fs, dst_fs)
+
+            self.assertEqual(copied, ["/file1.txt", "/one_level_down/file2.txt"])
+            self.assertTrue(dst_fs.exists("/file1.txt"))
+            self.assertTrue(dst_fs.exists("/one_level_down/file2.txt"))
+
+            src_fs.close()
+            dst_fs.close()
+
+        finally:
+            shutil.rmtree(src_dir)
+            shutil.rmtree(dst_dir)
+
+    def test_copy_fs_if_newer_dont_copy_when_dst_exists(self):
+        try:
+            #src is older than dst => no copy should be necessary
+            src_dir = self._create_sandbox_dir()
+            src_file1 = self._touch(src_dir, "file1.txt")
+            self._write_file(src_file1)
+
+            dst_dir = self._create_sandbox_dir()
+            dst_file1 = self._touch(dst_dir, "file1.txt")
+            self._write_file(dst_file1)
+
+            src_fs = open_fs('osfs://' + src_dir)
+            dst_fs = open_fs('osfs://' + dst_dir)
+
+            self.assertTrue(dst_fs.exists("/file1.txt"))
+
+            copied = fs.copy.copy_fs_if_newer(src_fs, dst_fs)
 
             self.assertEqual(copied, [])
-            self.assertTrue(dst_fs.exists(filename))
+            self.assertTrue(dst_fs.exists("/file1.txt"))
 
             src_fs.close()
             dst_fs.close()
@@ -96,28 +153,76 @@ class TestCopy(unittest.TestCase):
             shutil.rmtree(src_dir)
             shutil.rmtree(dst_dir)
 
-    def test_copy_when_src_is_newer(self):
+    def test_copy_dir_if_newer_one_dst_doesnt_exist(self):
         try:
 
-            filename = "file1.txt"
-            #create first dst ==> dst is older the src
-            dst_dir, _ = self._create_file(filename, 1024)
-            src_dir, _ = self._create_file(filename, 1024)
+            src_dir = self._create_sandbox_dir()
+            src_file1 = self._touch(src_dir, "file1.txt")
+            self._write_file(src_file1)
+
+            src_file2 = self._touch(src_dir, "one_level_down" + os.sep + "file2.txt")
+            self._write_file(src_file2)
+
+            dst_dir = self._create_sandbox_dir()
+            dst_file1 = self._touch(dst_dir, "file1.txt")
+            self._write_file(dst_file1)
 
             src_fs = open_fs('osfs://' + src_dir)
             dst_fs = open_fs('osfs://' + dst_dir)
 
-            self.assertTrue(dst_fs.exists(filename))
+            copied = fs.copy.copy_dir_if_newer(src_fs, "/", dst_fs, "/")
 
-            copied = fs.copy.copy_fs(src_fs, dst_fs, copy_if_newer=True)
-
-            self.assertEqual(copied, ["/" + filename])
-            self.assertTrue(dst_fs.exists(filename))
+            self.assertEqual(copied, ["/one_level_down/file2.txt"])
+            self.assertTrue(dst_fs.exists("/one_level_down/file2.txt"))
 
             src_fs.close()
             dst_fs.close()
-
         finally:
             shutil.rmtree(src_dir)
             shutil.rmtree(dst_dir)
 
+    def test_copy_dir_if_newer_same_fs(self):
+        try:
+            src_dir = self._create_sandbox_dir()
+            src_file1 = self._touch(src_dir, "src" + os.sep + "file1.txt")
+            self._write_file(src_file1)
+
+            dst_dir = self._create_sandbox_dir(home=src_dir)
+
+            src_fs = open_fs('osfs://' + src_dir)
+            # dst_fs = open_fs('osfs://' + dst_dir)
+
+            copied = fs.copy.copy_dir_if_newer(src_fs, "/src", src_fs, "/dst")
+
+            self.assertEqual(copied, ["/dst/file1.txt"])
+            self.assertTrue(src_fs.exists("/dst/file1.txt"))
+
+            src_fs.close()
+
+        finally:
+            shutil.rmtree(src_dir)
+
+    def test_copy_dir_if_newer_multiple_files(self):
+
+        try:
+            src_dir = self._create_sandbox_dir()
+            src_fs = open_fs('osfs://' + src_dir)
+            src_fs.makedirs('foo/bar')
+            src_fs.makedirs('foo/empty')
+            src_fs.touch('test.txt')
+            src_fs.touch('foo/bar/baz.txt')
+
+            dst_dir = self._create_sandbox_dir()
+            dst_fs = open_fs('osfs://' + dst_dir)
+
+            fs.copy.copy_dir_if_newer(src_fs, '/foo', dst_fs, '/')
+
+            self.assertTrue(dst_fs.isdir('bar'))
+            self.assertTrue(dst_fs.isdir('empty'))
+            self.assertTrue(dst_fs.isfile('bar/baz.txt'))
+        finally:
+            shutil.rmtree(src_dir)
+            shutil.rmtree(dst_dir)
+
+if __name__ == "__main__":
+    unittest.main()
