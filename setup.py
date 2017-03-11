d795b58ebc8be9ab5eac3744f20fc18326e24b55
Will McGugan <willmcgugan@gmail.com>
2017-03-11 20:32:23 +0000

    fix license

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('fs/_version.py') as f:
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
+    'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
