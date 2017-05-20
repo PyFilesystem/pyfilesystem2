a08162ed04733b681b03abb03e0417b20453ba7e
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-05-20 10:15:16 +0200

    Add a dynamically defined __all__ to fs.opener

--- fs/opener/__init__.py
+++ fs/opener/__init__.py
@@ -1,8 +1,22 @@
 import pkgutil
+import importlib
 __path__ = pkgutil.extend_path(__path__, __name__)
 
 from ._registry import registry, Registry
 from ._errors import OpenerError, ParseError, Unsupported
+
+
+__all__ = [
+    "registry",
+    "Registry",
+    "OpenerError",
+    "ParseError",
+    "Unsupported",
+    'open_fs',
+    'open',
+    'manage_fs',
+    'parse',
+]
 
 open_fs = registry.open_fs
 open = registry.open
@@ -12,3 +26,4 @@ parse = registry.parse
 for _, modname, _ in pkgutil.iter_modules(__path__):
     if not modname.startswith('_'):
         importlib.import_module('.'.join([__name__, modname]), package=__name__)
+        __all__.append(modname)
