1c83fcd923aea82ec3404bc644ac36c219abd028
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-30 18:52:38 +0200

    Add entry point related errors to fs.opener.errors

--- fs/opener/errors.py
+++ fs/opener/errors.py
@@ -13,6 +13,15 @@ class ParseError(ValueError):
 class OpenerError(Exception):
     """Base class for opener related errors."""
 
-
 class Unsupported(OpenerError):
     """May be raised by opener if the opener fails to open a FS."""
+
+
+class EntryPointError(Exception):
+    """Base class for entry point related errors."""
+
+class EntryPointLoadingError(EntryPointError, RuntimeError):
+    """Raised when an entry point could not be loaded."""
+
+class EntryPointTypeError(EntryPointError, TypeError):
+    """Raised when an entry point does not subclass ``fs.opener.base.Opener``"""
