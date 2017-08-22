712bd2c0583e66df92b53ee0d00b8006eeafb530
Will McGugan <willmcgugan@gmail.com>
2017-08-22 16:36:22 +0100

    expand user in osfs opener

--- fs/opener/osfs.py
+++ fs/opener/osfs.py
@@ -12,8 +12,8 @@ class OSFSOpener(Opener):
 
     def open_fs(self, fs_url, parse_result, writeable, create, cwd):
         from ..osfs import OSFS
-        from os.path import abspath, normpath, join
-        _path = abspath(join(cwd, parse_result.resource))
+        from os.path import abspath, expanduser, normpath, join
+        _path = abspath(join(cwd, expanduser(parse_result.resource)))
         path = normpath(_path)
         osfs = OSFS(path, create=create)
         return osfs
