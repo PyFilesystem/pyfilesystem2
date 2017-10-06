d22fba6eb536d974aeacc4df7ec934699b79c339
Martin Larralde <althonos@users.noreply.github.com>
2017-10-06 14:25:36 +0200

    Fix units in `fs.filesize` (#85)

--- fs/filesize.py
+++ fs/filesize.py
@@ -9,7 +9,7 @@ Functions for reporting filesizes
 from __future__ import division
 from __future__ import unicode_literals
 
-__all__ = ['traditional', 'decimal']
+__all__ = ['traditional', 'decimal', 'binary']
 
 
 
@@ -17,7 +17,7 @@ def _to_str(size, suffixes, base):
     try:
         size = int(size)
     except ValueError:
-        raise ValueError(
+        raise TypeError(
             "filesize requires a numeric value, not {!r}".format(size)
         )
     if size == 1:
@@ -35,7 +35,7 @@ def _to_str(size, suffixes, base):
 def traditional(size):
     """
     Convert a filesize in to a string representation with traditional
-    (base 2) units.
+    (base 2) units and JDEC prefixes.
 
     :param int size: A file size.
     :returns: A string containing a abbreviated file size and units.
@@ -49,18 +49,35 @@ def traditional(size):
     )
 
 
+def binary(size):
+    """
+    Convert a filesize in to a string representation with binary units
+    and SI binary prefixes.
+
+    :param int size: A file size.
+    :param bool si: True to use SI prefixes, False to use JDEC prefixes.
+    :returns: A string containing a abbreviated file size and units.
+    :rtype str:
+
+    """
+    return _to_str(
+        size,
+        ('KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'),
+        1024
+    )
+
+
 def decimal(size):
     """
     Convert a filesize in to a string representation with decimal
-    units.
+    units and SI decimal prefixes.
 
     :param int size: A file size.
     :returns: A string containing a abbreviated file size and units.
     :rtype str:
     """
-
     return _to_str(
         size,
-        ('kbit', 'Mbit', 'Gbit', 'Tbit', 'Pbit', 'Ebit', 'Zbit', 'Ybit'),
+        ('kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'),
         1000
     )
