b9c67f75f0486123c0d8712db17b72ebf4b44f78
merlink <merlink@gmx.de>
2018-02-11 15:13:05 +0100

    Problem seems to fs.time module...

--- fs/wrap.py
+++ fs/wrap.py
@@ -15,8 +15,8 @@ Here's an example that opens a filesystem then makes it *read only*::
 
 from __future__ import print_function
 from __future__ import unicode_literals
-import time
-from time import time as gettime
+
+from ..time import time as gettime
 from .wrapfs import WrapFS
 from .path import abspath, normpath, split
 from .errors import ResourceReadOnly, ResourceNotFound
