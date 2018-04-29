50c4ac4b390484167fc2eb75a4b2eacaca6a3b00
Will McGugan <willmcgugan@gmail.com>
2018-04-29 19:08:52 +0100

    cover exclude

--- fs/_typing.py
+++ fs/_typing.py
@@ -1,4 +1,3 @@
-# pragma: no cover
 """
 Typing objects missing from Python3.5.1
 
@@ -11,12 +10,12 @@ _PY = sys.version_info
 
 
 if _PY.major == 3 and _PY.minor == 5 and _PY.micro in (0, 1):
-    def overload(func):
+    def overload(func):  # pragma: no cover
         return func
 else:
     from typing import overload
 
 try:
     from typing import Text
-except ImportError:
+except ImportError:  # pragma: no cover
     Text = six.text_type
