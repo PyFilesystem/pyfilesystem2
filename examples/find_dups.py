a0d3aed19752a9b8a69364fb0995a5be50a94615
Will McGugan <willmcgugan@gmail.com>
2019-02-17 14:37:03 +0000

    use hash method in find_dups

--- examples/find_dups.py
+++ examples/find_dups.py
@@ -8,33 +8,20 @@ Usage:
 """
 
 from collections import defaultdict
-import hashlib
 import sys
 
 from fs import open_fs
 
 
-def get_hash(bin_file):
-    """Get the md5 hash of a file."""
-    file_hash = hashlib.md5()
-    while True:
-        chunk = bin_file.read(1024 * 1024)
-        if not chunk:
-            break
-        file_hash.update(chunk)
-    return file_hash.hexdigest()
-
-
 hashes = defaultdict(list)
 with open_fs(sys.argv[1]) as fs:
     for path in fs.walk.files():
-        with fs.open(path, "rb") as bin_file:
-            file_hash = get_hash(bin_file)
+        file_hash = fs.hash(path, "md5")
         hashes[file_hash].append(path)
 
 for paths in hashes.values():
     if len(paths) > 1:
         for path in paths:
-            print(f" {path}")
+            print(path)
         print()
 
