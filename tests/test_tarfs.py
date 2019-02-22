13c6c219619256a465bf84bf93dda46bfac72232
Will McGugan <willmcgugan@gmail.com>
2019-02-22 23:57:20 +0000

    close tempfiles

--- tests/test_tarfs.py
+++ tests/test_tarfs.py
@@ -9,6 +9,7 @@ import tarfile
 import getpass
 import tarfile
 import tempfile
+import time
 import unittest
 import uuid
 
@@ -26,6 +27,7 @@ from .test_archives import ArchiveTestCases
 class TestWriteReadTarFS(unittest.TestCase):
     def setUp(self):
         fh, self._temp_path = tempfile.mkstemp()
+        os.close(fh)
 
     def tearDown(self):
         os.remove(self._temp_path)
