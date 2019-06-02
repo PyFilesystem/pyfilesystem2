e07d82aed42cd055c7c95feaaa5b4e7e9945ecf7
Will McGugan <willmcgugan@gmail.com>
2019-06-02 17:06:20 +0100

    validate path in FTPFS.geturl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [2.5.6] - Unreleased
+
+### Added
+
+- Implemented geturl in FTPFS zmej-serow
+
 ## [2.4.5] - 2019-05-05
 
 ### Fixed
--- fs/ftpfs.py
+++ fs/ftpfs.py
@@ -453,13 +453,14 @@ class FTPFS(FS):
         """~ftplib.FTP: the underlying FTP client.
         """
         return self._get_ftp()
-    
+
     def geturl(self, path, purpose="download"):
         # type: (str, str) -> Text
         """Get FTP url for resource."""
+        _path = self.validatepath(path)
         if purpose != "download":
-            raise errors.NoURL(path, purpose, "No such purpose")
-        return "{}/{}".format(self.ftp_url, path)
+            raise errors.NoURL(_path, purpose)
+        return "{}{}".format(self.ftp_url, _path)
 
     def _get_ftp(self):
         # type: () -> FTP
