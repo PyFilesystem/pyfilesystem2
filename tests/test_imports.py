c783f352ec29e7cd122254e882b294810e96df3d
Will McGugan <willmcgugan@gmail.com>
2019-07-28 16:33:24 +0100

    fix test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,10 +5,13 @@ import unittest
 class TestImports(unittest.TestCase):
     def test_import_path(self):
         """Test import fs also imports other symbols."""
-        sys.modules.pop("fs")
-        import fs
+        restore_fs = sys.modules.pop("fs")
+        try:
+            import fs
 
-        fs.path
-        fs.Seek
-        fs.ResourceType
-        fs.open_fs
+            fs.path
+            fs.Seek
+            fs.ResourceType
+            fs.open_fs
+        finally:
+            sys.modules["fs"] = restore_fs
