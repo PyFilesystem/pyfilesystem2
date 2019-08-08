ab7ec66ec043fad4107fe38eafbbcf031461059b
chfw <wangc_2011@hotmail.com>
2019-08-08 07:11:49 +0100

    :tractor: alternative file uri implementation

--- fs/osfs.py
+++ fs/osfs.py
@@ -586,15 +586,10 @@ class OSFS(FS):
             raise NoURL(path, purpose)
 
         sys_path = self.getsyspath(path)
-        if _WINDOWS_PLATFORM and ':' in sys_path:
-            drive_letter, path = sys_path.split(':', 1)
-            path = path.replace("\\", "/")
-            path = six.moves.urllib.parse.quote(path)
-            url_path = "{}:{}".format(drive_letter, path)
-        else:
-            sys_path = sys_path.replace("\\", "/")
-            url_path = six.moves.urllib.parse.quote(sys_path)
-        return "file://" + url_path
+        url_path = six.moves.urllib.parse.urljoin(
+            'file:',
+            six.moves.urllib.request.pathname2url(sys_path))
+        return url_path
 
     def gettype(self, path):
         # type: (Text) -> ResourceType
