f2fe73516767e5708394ddd9d06a11a7a2585d33
chfw <wangc_2011@hotmail.com>
2019-08-08 23:02:14 +0100

    :bug: forward slash on Windows is a valid path separator

--- tests/test_base.py
+++ tests/test_base.py
@@ -79,19 +79,22 @@ class TestBase(unittest.TestCase):
             ["foo/bar ha/barz", "foo/bar%20ha/barz"],
             ["example b.txt", "example%20b.txt"],
             ["exampleㄓ.txt", "example%E3%84%93.txt"],
-            ["test/forward\\slash", "test/forward%5Cslash"],
         ]
         if platform.system() == "Windows":
             test_fixtures.extend(
                 [
                     ["C:\\My Documents\\test.txt", "C:/My%20Documents/test.txt"],
                     ["C:/My Documents/test.txt", "C:/My%20Documents/test.txt"],
+                    # on Windows '\' is regarded as path separator
+                    ["test/forward\\slash", "test/forward/slash"],
                 ]
             )
         else:
-            test_fixtures.append(
+            test_fixtures.extend([
                 # colon:tmp is bad path under Windows
                 ["test/colon:tmp", "test/colon%3Atmp"]
-            )
+                # Unix treat \ as %5C                
+                ["test/forward\\slash", "test/forward%5Cslash"],
+            ])
         for test_snippet, expected in test_fixtures:
             self.assertEqual(FS.quote(test_snippet), expected)
