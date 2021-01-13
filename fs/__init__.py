1e1a6c94a4c222aaec7a637eaa6cb662fe2ba460
toddg <toddg@zwrob.com>
2021-01-13 04:47:36 -0800

    Add 'base' to 'fs' package

--- fs/__init__.py
+++ fs/__init__.py
@@ -8,5 +8,6 @@ from .enums import ResourceType, Seek
 from .opener import open_fs
 from ._fscompat import fsencode, fsdecode
 from . import path
+from . import base
 
-__all__ = ["__version__", "ResourceType", "Seek", "open_fs"]
+__all__ = ["__version__", "ResourceType", "Seek", "open_fs", "base"]
