4feb2425bc220716f0beb2fa313c9ebfc1ec65e1
Martin Larralde <martin.larralde@embl.de>
2021-07-03 11:53:59 +0200

    Remove debug print from `tests.test_ftpfs`

--- tests/test_ftpfs.py
+++ tests/test_ftpfs.py
@@ -266,7 +266,6 @@ class TestFTPFS(FSTestCases, unittest.TestCase):
         self.fs.create("bar")
         mtime_detail = self.fs.getinfo("bar", ("basic", "details")).modified
         mtime_modified = self.fs.getmodified("bar")
-        print(mtime_detail, mtime_modified)
         # Microsecond and seconds might not actually be supported by all
         # FTP commands, so we strip them before comparing if it looks
         # like at least one of the two values does not contain them.
