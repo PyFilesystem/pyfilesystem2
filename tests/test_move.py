1267194f785fa09738784fb4fa004db30e19f460
Thomas Feldmann <mail@tfeldmann.de>
2022-03-25 13:51:33 +0100

    don't run tests twice in parameterized_class

--- tests/test_move.py
+++ tests/test_move.py
@@ -12,7 +12,7 @@ from fs.wrap import read_only
 
 
 @parameterized_class(("preserve_time",), [(True,), (False,)])
-class TestMove(unittest.TestCase):
+class TestMovePreserveTime(unittest.TestCase):
     def test_move_fs(self):
         namespaces = ("details", "modified")
 
@@ -40,6 +40,32 @@ class TestMove(unittest.TestCase):
             self.assertEqual(dst_file1_info.modified, src_file1_info.modified)
             self.assertEqual(dst_file2_info.modified, src_file2_info.modified)
 
+    def test_move_dir(self):
+        namespaces = ("details", "modified")
+
+        src_fs = open_fs("mem://")
+        src_fs.makedirs("foo/bar")
+        src_fs.touch("test.txt")
+        src_fs.touch("foo/bar/baz.txt")
+        src_file2_info = src_fs.getinfo("foo/bar/baz.txt", namespaces)
+
+        dst_fs = open_fs("mem://")
+        dst_fs.create("test.txt")
+        dst_fs.setinfo("test.txt", {"details": {"modified": 1000000}})
+
+        fs.move.move_dir(src_fs, "/foo", dst_fs, "/", preserve_time=self.preserve_time)
+
+        self.assertTrue(dst_fs.isdir("bar"))
+        self.assertTrue(dst_fs.isfile("bar/baz.txt"))
+        self.assertFalse(src_fs.exists("foo"))
+        self.assertTrue(src_fs.isfile("test.txt"))
+
+        if self.preserve_time:
+            dst_file2_info = dst_fs.getinfo("bar/baz.txt", namespaces)
+            self.assertEqual(dst_file2_info.modified, src_file2_info.modified)
+
+
+class TestMove(unittest.TestCase):
     def test_move_file(self):
         with open_fs("temp://") as temp:
             syspath = temp.getsyspath("/")
@@ -106,26 +132,22 @@ class TestMove(unittest.TestCase):
             )
             self.assertTrue(src.exists("file.txt"))
 
-    def test_move_dir(self):
-        namespaces = ("details", "modified")
-
+    def test_move_dir_cleanup(self):
         src_fs = open_fs("mem://")
         src_fs.makedirs("foo/bar")
-        src_fs.touch("test.txt")
         src_fs.touch("foo/bar/baz.txt")
-        src_file2_info = src_fs.getinfo("foo/bar/baz.txt", namespaces)
+        src_fs.touch("foo/test.txt")
 
         dst_fs = open_fs("mem://")
         dst_fs.create("test.txt")
-        dst_fs.setinfo("test.txt", {"details": {"modified": 1000000}})
 
-        fs.move.move_dir(src_fs, "/foo", dst_fs, "/", preserve_time=self.preserve_time)
+        ro_src = read_only(src_fs)
 
-        self.assertTrue(dst_fs.isdir("bar"))
-        self.assertTrue(dst_fs.isfile("bar/baz.txt"))
-        self.assertFalse(src_fs.exists("foo"))
-        self.assertTrue(src_fs.isfile("test.txt"))
+        with self.assertRaises(ResourceReadOnly):
+            fs.move.move_dir(ro_src, "/foo", dst_fs, "/")
 
-        if self.preserve_time:
-            dst_file2_info = dst_fs.getinfo("bar/baz.txt", namespaces)
-            self.assertEqual(dst_file2_info.modified, src_file2_info.modified)
+        self.assertTrue(src_fs.exists("foo/bar/baz.txt"))
+        self.assertTrue(src_fs.exists("foo/test.txt"))
+        self.assertFalse(dst_fs.isdir("bar"))
+        self.assertFalse(dst_fs.exists("bar/baz.txt"))
+        self.assertTrue(dst_fs.exists("test.txt"))
